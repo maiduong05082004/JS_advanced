21e4aa3e82725f3adf5b84d5f22edce5587fbf17
maiduong <duongmdph40323@gmail.com>
2024-02-27 22:50:40 +0700

    Operator Precedence

---
+++ starter/script.js
@@ -0,0 +1,59 @@
+// let js="amazing";
+// if(js==="amazing") alert("javascript is Fun!");
+
+// console.log(40+8+2);
+// console.log("janas");
+// let fistName="Jana";
+// console.log(fistName);
+// let PI=3.232;
+// console.log(PI);
+// let sex=true;
+
+// if(sex==true){
+//     console.log("Male");
+// }else{
+//     console.log( "Female");
+// }
+// console.log(typeof true);
+// let test="hihi";
+// console.log(typeof test);
+// let years;
+// console.log(typeof years);
+// let number=15;
+// console.log(typeof number)
+// console.log(typeof null);
+
+// let age=30;
+// age=31;
+// console.log(age);
+// const job="Software Engineer";
+// // job="Data Scientest";
+// console.log(job);
+
+// var birthYear=1991;
+// birthYear=1990;
+// console.log(birthYear);
+
+// lastName="Lee";
+// console.log(lastName);
+const now=2037;
+const ageJone=now-2022;
+const ageMai=now-2033;
+console.log(ageJone,ageMai);
+console.log(ageJone*2,ageMai/10,2**3);
+//2 mũ 3
+//toán tử gán
+const fistName="mai";
+const lastName="duong";
+console.log(fistName + '' + lastName);
+let x= 10+5;
+x+=10; //x=x+10=25
+x-=10;   //x=x-10=15
+x*=4;   //x=x*4=60
+x++;          //x =x+1=61
+x--;         //x=x-
+console.log(x);
+
+console.log(ageJone>ageMai);
+console.log(ageJone>=18);
+
